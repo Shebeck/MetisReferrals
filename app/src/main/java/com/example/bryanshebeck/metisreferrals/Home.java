c4bca263903aecb867344a43184198789670fe7a
Basil Kuloba <bwkuloba@MacBook-Pro.local>
2017-04-08 12:31:51 -0500

    added home pages

--- app/src/main/java/com/example/bryanshebeck/metisreferrals/Home.java
+++ app/src/main/java/com/example/bryanshebeck/metisreferrals/Home.java
@@ -3,11 +3,11 @@ package com.example.bryanshebeck.metisreferrals;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Home extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_splashpage);
+        setContentView(R.layout.home);
     }
 }
