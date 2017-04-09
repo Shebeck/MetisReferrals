7880fbb6831edf14598aae2073afd10596a90b50
johnstevens14 <jmstevens1899@gmail.com>
2017-04-09 12:38:23 -0500

    hooked up ratings page

--- app/src/main/java/com/example/bryanshebeck/metisreferrals/LeaveReview.java
+++ app/src/main/java/com/example/bryanshebeck/metisreferrals/LeaveReview.java
@@ -1,7 +1,10 @@
 package com.example.bryanshebeck.metisreferrals;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LeaveReview extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class LeaveReview extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.leave_review);
+
+        Button submitReviewButton = (Button) findViewById(R.id.leaveReview);
+        submitReviewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LeaveReview.this, Home.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/bryanshebeck/metisreferrals/doctorBio.java
+++ app/src/main/java/com/example/bryanshebeck/metisreferrals/doctorBio.java
@@ -1,7 +1,10 @@
 package com.example.bryanshebeck.metisreferrals;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class doctorBio extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class doctorBio extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_bio);
+
+        Button reviewButton = (Button) findViewById(R.id.leaveReview);
+        reviewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(doctorBio.this, LeaveReview.class));
+            }
+        });
     }
 }
