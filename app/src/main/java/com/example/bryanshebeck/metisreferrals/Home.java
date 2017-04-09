da90998a35cf939fa1d12af4e302aa08710305d2
johnstevens14 <jmstevens1899@gmail.com>
2017-04-08 20:23:24 -0500

    added buttons to home page

--- app/src/main/java/com/example/bryanshebeck/metisreferrals/Home.java
+++ app/src/main/java/com/example/bryanshebeck/metisreferrals/Home.java
@@ -1,7 +1,10 @@
 package com.example.bryanshebeck.metisreferrals;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Home extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
+
+        Button myApptButton = (Button) findViewById(R.id.myAppt);
+        myApptButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(Home.this, MyAppointments.class));
+            }
+        });
+
     }
 }
