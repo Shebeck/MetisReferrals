e2be28b2b85759b5c64a2c8657ba9615cc5c4a8f
Basil Kuloba <bwkuloba@MacBook-Pro.local>
2017-04-08 20:16:02 -0500

    added java files for mydoctors, settings, profiles, about, and login

---
+++ app/src/main/java/com/example/bryanshebeck/metisreferrals/Profile.java
@@ -0,0 +1,18 @@
+package com.example.bryanshebeck.metisreferrals;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by bwkuloba on 4/8/17.
+ */
+
+public class Profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile);
+    }
+}
