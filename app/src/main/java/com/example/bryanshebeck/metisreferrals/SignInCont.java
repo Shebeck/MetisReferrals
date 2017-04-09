0cbca5ac3325e43f4a0beade38b6480d2a77297f
johnstevens14 <jmstevens1899@gmail.com>
2017-04-09 01:45:20 -0500

    Polished sign in page

---
+++ app/src/main/java/com/example/bryanshebeck/metisreferrals/SignInCont.java
@@ -0,0 +1,13 @@
+package com.example.bryanshebeck.metisreferrals;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignInCont extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signin_cont);
+    }
+}
