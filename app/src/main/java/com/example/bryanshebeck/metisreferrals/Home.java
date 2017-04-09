a0b130fe4dc001707db938d1facacd4edc255c5e
Basil Kuloba <bwkuloba@MacBook-Pro.local>
2017-04-09 01:21:18 -0500

    added functionality to home screen buttons

--- app/src/main/java/com/example/bryanshebeck/metisreferrals/Home.java
+++ app/src/main/java/com/example/bryanshebeck/metisreferrals/Home.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Home extends AppCompatActivity {
 
@@ -12,6 +13,39 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
+
+        ImageButton homeProfileButton = (ImageButton) findViewById(R.id.homeScreenProfileButton);
+        homeProfileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Home.this, Profile.class));
+            }
+        });
+
+        ImageButton homeDoctorsButton = (ImageButton) findViewById(R.id.homeScreenMyDocsButton);
+        homeDoctorsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Home.this, MyDoctors.class));
+            }
+        });
+
+        ImageButton homeAppointmentsButton = (ImageButton) findViewById(R.id.homeScreenAppointmentsButton);
+        homeAppointmentsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Home.this, MyAppointments.class));
+            }
+        });
+
+        ImageButton homeAboutUsButton = (ImageButton) findViewById(R.id.homeScreenAboutButton);
+        homeAboutUsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Home.this, About.class));
+            }
+        });
+
 
 
     }
