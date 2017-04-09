ec3c2ce14fdc65fba27e83dfd4d2d78e589f7886
Basil Kuloba <bwkuloba@MacBook-Pro.local>
2017-04-09 02:47:18 -0500

    finished sign in continued, fixed half of the string errors, and added asterisks to passowrd input

--- app/src/main/java/com/example/bryanshebeck/metisreferrals/SignIn.java
+++ app/src/main/java/com/example/bryanshebeck/metisreferrals/SignIn.java
@@ -2,6 +2,9 @@ package com.example.bryanshebeck.metisreferrals;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.PasswordTransformationMethod;
+import android.view.View;
+import android.widget.EditText;
 
 public class SignIn extends AppCompatActivity {
 
@@ -9,5 +12,31 @@ public class SignIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signin);
+
+        EditText edittext = (EditText)findViewById(R.id.passResp1);
+        edittext.setTransformationMethod(new AsteriskPasswordTransformationMethod());
     }
+
+    public class AsteriskPasswordTransformationMethod extends PasswordTransformationMethod {
+        @Override
+        public CharSequence getTransformation(CharSequence source, View view) {
+            return new PasswordCharSequence(source);
+        }
+
+        private class PasswordCharSequence implements CharSequence {
+            private CharSequence mSource;
+            public PasswordCharSequence(CharSequence source) {
+                mSource = source; // Store char sequence
+            }
+            public char charAt(int index) {
+                return '*'; // This is the important part
+            }
+            public int length() {
+                return mSource.length(); // Return default
+            }
+            public CharSequence subSequence(int start, int end) {
+                return mSource.subSequence(start, end); // Return default
+            }
+        }
+    };
 }
